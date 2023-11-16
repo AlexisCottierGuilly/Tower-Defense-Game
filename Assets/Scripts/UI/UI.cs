96d1ee91d4fd53d9755b63418d595187c71fbdf9
EnergieMic <energiemic@gmail.com>
2023-11-16 15:57:20 -0500

    Un bouton d'ajouté
    Besoin de l'attacher a quelque chose dans Unity pour que la fonction soit executée.

---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public void Clicked()
+    {
+        print("Start");
+    }
+}
