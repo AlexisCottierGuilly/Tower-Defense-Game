6eaca9790c808fcf4c9a2d743b60329ba2ec4dc8
EnergieMic <energiemic@gmail.com>
2023-12-15 16:16:06 -0500

    You dont need to start the game from menu now

---
+++ Assets/Scripts/UI/safe.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class safe : MonoBehaviour
+{
+    void Start()
+    {
+        if (0.1 >= Time.time)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
