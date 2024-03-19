ba12b5c08fd43c0daa3b5486cf401629a15f7b74
EnergieMic <energiemic@gmail.com>
2024-03-19 12:00:47 -0400

    started wall (next: health bar)

---
+++ Assets/Scripts/Terrain Generation/Walls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Walls : MonoBehaviour
+{
+    void Start()
+    {
+        print(GameManager.instance.mapSize.x);
+        print(GameManager.instance.mapSize.y);
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+    }
+}
