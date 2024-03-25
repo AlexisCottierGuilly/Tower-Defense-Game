e3281dcd29c6827a5869c888d3d1392d97f8c9e0
EnergieMic <energiemic@gmail.com>
2024-03-25 14:29:35 -0400

    1

--- Assets/Scripts/Terrain Generation/Walls.cs
+++ Assets/Scripts/Terrain Generation/Walls.cs
@@ -7,12 +7,16 @@ public class Walls : MonoBehaviour
 {
     void Start()
     {
-        print(GameManager.instance.mapSize.x);
-        print(GameManager.instance.mapSize.y);
+        /*GameObject plane1  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        plane1.transform.position = new Vector3(0,100,0);
+        plane1.transform.localScale = new Vector2(GameManager.instance.mapSize.x*400,200);
+        GameObject plane2  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        GameObject plane3  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        GameObject plane4  = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        
         foreach (Transform child in transform)
         {
             Destroy(child.gameObject);
-        }
-
+        }*/
     }
 }
