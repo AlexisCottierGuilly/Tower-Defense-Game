2ab49a6ee07c6f8ccb14ef48f9bf2417aa572020
EnergieMic <energiemic@gmail.com>
2024-04-09 15:23:05 -0400

    gold finish

--- Assets/Scripts/UI/gold.cs
+++ Assets/Scripts/UI/gold.cs
@@ -5,15 +5,10 @@ using TMPro;
 
 public class gold : MonoBehaviour
 {
-    private int last = GameManager.instance.gold;
-    public TMP_Text Text;
+    public TextMeshProUGUI text;
 
     void Update()
     {
-        if (GameManager.instance.gold != last)
-        {
-            //Text.TMP_Text = "Gold: " + GameManager.instance.gold.ToString();
-            last = GameManager.instance.gold;
-        }
+        text.text = "Gold: " + GameManager.instance.gold.ToString();
     }
 }
